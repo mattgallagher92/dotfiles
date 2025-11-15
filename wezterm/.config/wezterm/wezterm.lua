c0b19e428805d938da2db7c40df30d320dcfae17
Matt Gallagher <mjg1992@hotmail.co.uk>
2025-11-15 11:50:08 +0000

    Have WezTerm react to system light/dark mode

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,23 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
+-- See https://wezterm.org/config/lua/wezterm.gui/get_appearance.html
+function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font({
 	family = "JetBrains Mono",
