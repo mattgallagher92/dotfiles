1d08aea26303689a7ca3dda74bc3ffd02b3e0433
Matt Gallagher <mjg1992@hotmail.co.uk>
2026-04-27 22:37:33 +0100

    Add adventofneovim module (up to end of day 3)
    https://youtube.com/playlist?list=PLep05UYkc6wTyBe7kPjQFWVXTlhKeQejM

---
+++ adventofneovim/.config/adventofneovim/init.lua
@@ -0,0 +1,13 @@
+print 'advent of neovim'
+
+vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
+vim.keymap.set('n', '<space>x', ':.lua<CR>')
+vim.keymap.set('v', '<space>x', ':lua<CR>')
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight after yanking text',
+  group = vim.api.nvim_create_augroup('aon-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
