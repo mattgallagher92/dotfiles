6b6580a1f7f4483dea3de35b3ec1503b5491015d
Matt Gallagher <mjg1992@hotmail.co.uk>
2026-05-10 22:21:20 +0100

    adventofneovim day 8 (basic telescope setup)

---
+++ adventofneovim/.config/adventofneovim/lua/config/plugins/telescope.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  opts = {
+    defaults = {
+      layout_strategy = "bottom_pane",
+      layout_config = {
+        height = 0.9,
+      },
+    },
+    pickers = {
+      find_files = {
+        -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+        find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+      },
+    },
+  },
+  keys = {
+    { "<leader>sf", "<cmd>Telescope find_files<CR>" },
+    { "<leader>sh", "<cmd>Telescope help_tags<CR>" },
+    { "<leader>sg", "<cmd>Telescope live_grep<CR>" },
+    { "<leader>sb", "<cmd>Telescope buffers<CR>" },
+  },
+}
