15818815910cf6861080294a08dc31e4db92bfac
Matt Gallagher <mjg1992@hotmail.co.uk>
2026-05-11 22:01:45 +0100

    adventofneovim day 11 (quickfix)

--- adventofneovim/.config/adventofneovim/init.lua
+++ adventofneovim/.config/adventofneovim/init.lua
@@ -11,6 +11,9 @@ vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
 vim.keymap.set('n', '<space>x', ':.lua<CR>')
 vim.keymap.set('v', '<space>x', ':lua<CR>')
 
+vim.keymap.set('n', '<M-n>', '<cmd>cnext<CR>')
+vim.keymap.set('n', '<M-p>', '<cmd>cprev<CR>')
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight after yanking text',
   group = vim.api.nvim_create_augroup('aon-highlight-yank', { clear = true }),
