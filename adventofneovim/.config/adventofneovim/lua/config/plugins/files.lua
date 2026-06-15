d3c89c9fb2af4e8f06afb02efb913b7a7e3e080d
Matt Gallagher <mjg1992@hotmail.co.uk>
2026-06-15 22:23:46 +0100

    Update oil opts

--- adventofneovim/.config/adventofneovim/lua/config/plugins/files.lua
+++ adventofneovim/.config/adventofneovim/lua/config/plugins/files.lua
@@ -3,11 +3,25 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
-    keys = { { '<leader>-', vim.cmd.Oil, desc = 'Oil' } },
-    -- Optional dependencies
-    -- dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    opts = {
+      skip_confirm_for_simple_edits = true,
+      watch_for_changes = true,
+      use_default_keymaps = true,
+      keymaps = {
+        ['gy'] = 'actions.yank_entry',
+      },
+      view_options = {
+        show_hidden = true,
+        is_hidden_file = function(name)
+          return vim.startswith(name, '.')
+        end,
+        is_always_hidden = function(name)
+          return name == '..'
+        end,
+      },
+    },
+    keys = { { '<leader>-', vim.cmd.Oil, desc = 'Open parent directory in oil' } },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
   }
