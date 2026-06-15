5846ca68677e27ef804d14202e78f1246a39b3aa
Matt Gallagher <mjg1992@hotmail.co.uk>
2026-06-15 22:23:47 +0100

    Add diagnostics keymaps

--- adventofneovim/.config/adventofneovim/lua/config/plugins/diagnostics.lua
+++ adventofneovim/.config/adventofneovim/lua/config/plugins/diagnostics.lua
@@ -1,4 +1,24 @@
-vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+local d = vim.diagnostic
+
+local function jumper(opts)
+  return function()
+    d.jump(opts)
+  end
+end
+
+vim.keymap.set("n", "<leader>dnd", jumper({ count = 1 }), { desc = "[D]iagnostics [n]ext [d]iagnostic" })
+vim.keymap.set("n", "<leader>dpd", jumper({ count = -1 }), { desc = "[D]iagnostics [p]revious [d]iagnostic" })
+vim.keymap.set("n", "<leader>dne", jumper({ count = 1, severity = d.severity.ERROR }),
+  { desc = "[D]iagnostics [n]ext [e]rror" })
+vim.keymap.set("n", "<leader>dpe", jumper({ count = -1, severity = d.severity.ERROR }),
+  { desc = "[D]iagnostics [p]revious [e]rror" })
+vim.keymap.set("n", "<leader>dnw", jumper({ count = 1, severity = d.severity.WARN }),
+  { desc = "[D]iagnostics [n]ext [w]arning" })
+vim.keymap.set("n", "<leader>dpw", jumper({ count = -1, severity = d.severity.WARN }),
+  { desc = "[D]iagnostics [p]revious [w]arning" })
+
+-- Disable Neovim's default virtual text diagnostics in favour of tiny-inline-diagnostic
+d.config({ virtual_text = false })
 
 return {
   "rachartier/tiny-inline-diagnostic.nvim",
