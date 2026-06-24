bc69eeb5c2058130758d4abcb217c003af63403f
Matt Gallagher <mjg1992@hotmail.co.uk>
2026-06-24 21:30:22 +0100

    Remove workaround for fixed bug
    Bug: https://github.com/neovim/neovim/issues/36257
    Fix: https://github.com/neovim/neovim/pull/40339

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,15 +1,3 @@
 -- LSP
--- Other LSP configs enabled via Mason LSP config. I want fine-grained control over fsautocomplete.
-vim.lsp.enable({ 'fsautocomplete' })
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('aon-lsp', { clear = true }),
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    -- fsautocomplete returns invalid data, which Neovim doesn't handle well, causing the editor to lock up.
-    -- (See https://github.com/mattgallagher92/fsac-semantic-tokens-bug-repro).
-    -- Workaround from https://github.com/neovim/neovim/issues/36257.
-    if client and client.server_capabilities then
-      client.server_capabilities.semanticTokensProvider = nil
-    end
-  end,
-})
+-- I want fine-grained control over fsautocomplete so manage it outside of Mason LSP config.
+vim.lsp.enable({ "fsautocomplete" })
