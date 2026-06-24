1896ea42ff4c4473d637f288a2572c748e7d6519
Matt Gallagher <mjg1992@hotmail.co.uk>
2026-06-24 21:44:52 +0100

    formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,10 @@
-print 'advent of neovim'
+print("Sourcing Matt's config")
 
 -- Must be set before loading plugins so that key maps use correct leader keys.
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true
@@ -12,22 +12,24 @@ vim.opt.smartcase = true
 vim.opt.shiftwidth = 4
 vim.opt.undofile = true
 
-vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<CR>')
-vim.keymap.set('n', '<leader>x', ':.lua<CR>')
-vim.keymap.set('v', '<leader>x', ':lua<CR>')
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<leader>x", ":.lua<CR>")
+vim.keymap.set("v", "<leader>x", ":lua<CR>")
 
-vim.keymap.set('n', '<M-n>', '<cmd>cnext<CR>')
-vim.keymap.set('n', '<M-p>', '<cmd>cprev<CR>')
+vim.keymap.set("n", "<M-n>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-p>", "<cmd>cprev<CR>")
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight after yanking text',
-  group = vim.api.nvim_create_augroup('aon-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight after yanking text",
+  group = vim.api.nvim_create_augroup("mattg-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
 
-require('config.lazy')
-require('config.treesitter')
-require('config.lsp')
-require('config.terminal')
+require("config.lazy")
+require("config.treesitter")
+require("config.lsp")
+require("config.terminal")
+
+print("Matt's config sourced")
--- nvim/.config/nvim/lua/config/treesitter.lua
+++ nvim/.config/nvim/lua/config/treesitter.lua
@@ -1,7 +1,7 @@
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'fsharp',
-  desc = 'Enable Tree-sitter in fsharp files',
-  group = vim.api.nvim_create_augroup('aon-fsharp', { clear = true }),
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "fsharp",
+  desc = "Enable Tree-sitter in fsharp files",
+  group = vim.api.nvim_create_augroup("mattg-fsharp", { clear = true }),
   callback = function()
     vim.treesitter.start()
   end,
