aee266e966588881ad3349bc149ca5ee2003c795
Matt Gallagher <mjg1992@hotmail.co.uk>
2026-06-24 18:48:18 +0100

    Set up mini.surround and mini.ai

--- nvim/.config/nvim/lua/config/plugins/mini.lua
+++ nvim/.config/nvim/lua/config/plugins/mini.lua
@@ -1,7 +1,9 @@
+# TODO: mini.ai and mini.surround
 return { 
   'nvim-mini/mini.nvim',
   config = function()
-    local statusline = require 'mini.statusline'
-    statusline.setup()
+     require('mini.ai').setup()
+     require('mini.surround').setup()
+     require('mini.statusline').setup()
   end
 }
